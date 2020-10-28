1568c12cc20408e1bba510962364f55d1f3f9ec5
Андрей Лисин <foxers100@gmail.com>
2020-10-28 23:16:50 +0300

    Deleting trash

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/foxers/CLionProjects/adv_cpp_homework_2
-# Build directory: /home/foxers/CLionProjects/adv_cpp_homework_2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtests "./tests")
-set_tests_properties(gtests PROPERTIES  _BACKTRACE_TRIPLES "/home/foxers/CLionProjects/adv_cpp_homework_2/CMakeLists.txt;21;add_test;/home/foxers/CLionProjects/adv_cpp_homework_2/CMakeLists.txt;0;")
